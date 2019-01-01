c33acc3c0aa27575080fea1f0de0f17f95bfb97d
cyrixmorten <cyrixmorten@gmail.com>
2019-01-01 18:42:53 +0100

    Scheduled time with year component (#8) (#9)
    * Scheduled time with year component (#8)
    
    * Using taskGroupTime when adjusting time for schedule check
    Before this the year component was not included, meaning that schedule check failed when entering a new year
    
    * Improved scheduled time calculation by taking createdAt on TaskGroupStarted out of the equation, instead the planned reset time of TaskGroup is used, making determining if within scheduled time deterministic even if TaskGroups are being forced reset mid tour.
    
    * Bumped version to 5.2.1

--- guardSwift/src/main/java/com/guardswift/persistence/parse/execution/task/TaskGroup.java
+++ guardSwift/src/main/java/com/guardswift/persistence/parse/execution/task/TaskGroup.java
@@ -6,11 +6,19 @@ import com.parse.ParseClassName;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import org.joda.time.DateTime;
+import org.joda.time.LocalDateTime;
+
+import java.time.DayOfWeek;
+import java.util.Date;
+
 @ParseClassName("TaskGroup")
 public class TaskGroup extends ExtendedParseObject {
 
 
 	public static final String name = "name";
+	public static final String createdDay = "createdDay";
+	public static final String timeResetDate = "timeResetDate";
 
 
 	@SuppressWarnings("unchecked")
@@ -30,6 +38,31 @@ public class TaskGroup extends ExtendedParseObject {
 
 	public ParseObject getOwner() {
 		return getParseObject(owner);
+	}
+
+	private Date getTimeResetDate() {
+		return getDate(TaskGroup.timeResetDate);
+	}
+
+	private int getCreatedDay() {
+		int dayOfWeek =  getInt(TaskGroup.createdDay);
+
+		// in javascript 0 is sunday
+		// in java 7 is sunday
+		if (dayOfWeek == 0) {
+			return 7;
+		}
+
+		return dayOfWeek;
+	}
+
+	public Date getAdjustedResetDate() {
+		DateTime hourAndMinuteReset = new DateTime(getTimeResetDate());
+
+		return DateTime.now()
+				.withDayOfWeek(getCreatedDay())
+				.withHourOfDay(hourAndMinuteReset.getHourOfDay())
+				.withMinuteOfHour(hourAndMinuteReset.getMinuteOfHour()).toDate();
 	}
 
 }
